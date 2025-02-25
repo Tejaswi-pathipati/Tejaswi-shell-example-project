1164bec998c8f43ecfe826931e7e27ad2f9684b3
Tejaswi <pathipati.tejaswi99@gmail.com>
2025-02-25 11:33:18 +0530

    This is my fully modified 3rd version of calci appn

--- calci.sh/calculator.sh
+++ calci.sh/calculator.sh
@@ -1,7 +1,11 @@
-This is calculator applocation
+This is calculator application
+This is addition
 x = a+b
+This is subraction
 y = a-b
-z = a/b
-a = a*b 
+This is Multiplication
+z = a*b
+This is division
+a = a/b 
 where a, b are 2 variables to store the values
 This is 4  step calculation
